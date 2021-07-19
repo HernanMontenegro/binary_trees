f73e58a913723efea62437d5f7c9b5115d46cea2
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-07-19 22:53:36 +0000

    stress +50

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    binary_tree_t *root = NULL;
+
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+    root = node->parent->parent;
+
+	if (root->left == node->parent)
+        return (root->right);
+    else if (root->right == node->parent)
+        return (root->left);
+
+	return (NULL);
+}
