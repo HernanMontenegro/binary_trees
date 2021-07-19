b59832fc07b4b860610a79f27c2ed15ca5539f52
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-07-19 22:54:27 +0000

    18 fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,17 +3,17 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    binary_tree_t *root = NULL;
+	binary_tree_t *root = NULL;
 
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-    root = node->parent->parent;
+	root = node->parent->parent;
 
 	if (root->left == node->parent)
-        return (root->right);
-    else if (root->right == node->parent)
-        return (root->left);
+		return (root->right);
+	else if (root->right == node->parent)
+		return (root->left);
 
 	return (NULL);
 }
