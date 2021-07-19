51a392d509ff4fbdb45d654fcd2b43144a82054c
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-07-19 19:15:25 +0000

    9 done

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left = 0, right = 0;
+
+    if (!tree)
+        return (0);
+    
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
+
+    if (left > right)
+        return (left) + 1;
+    return (right) + 1;
+}
